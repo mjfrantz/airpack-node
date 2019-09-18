5c4f1ec1187272e3d035feebd21e692ef1a0a61f
Ivan Garcia <ivanvlora@gmail.com>
2019-09-18 17:02:21 +0900

    implemented cors | changed port for env

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const morgan = require('morgan');
-const packRouter = require('./routes/packRoutes');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const packRouter = require("./routes/packRoutes");
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-//Middleware 
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+//Middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use((req, res, next) => {
-    console.log('Hello from Middleware');
-    next();
+  console.log("Hello from Middleware");
+  next();
 });
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-//Mounting the Router 
-app.use('/api/v1/packs', packRouter);
+//Mounting the Router
+app.use("/api/v1/packs", packRouter);
 
 module.exports = app;
--- controllers/packController.js
+++ controllers/packController.js
@@ -1,136 +1,135 @@
-const Pack = require('./../models/packModel');
+const Pack = require("./../models/packModel");
 
 exports.getAllPacks = async (req, res) => {
-    try {
-        //Build Query
-        //1) Filtering
-        const queryObj = {
-            ...req.query
-        };
-        const excludeFields = ['page', 'sort', 'limit', 'fields'];
-        excludeFields.forEach(el => delete queryObj[el]);
+  try {
+    //Build Query
+    //1) Filtering
+    const queryObj = {
+      ...req.query
+    };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach(el => delete queryObj[el]);
 
-        //1b) Advanced Filtering 
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    //1b) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-        let query = Pack.find(JSON.parse(queryStr));
+    let query = Pack.find(JSON.parse(queryStr));
 
-        //2) Sorting
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort('price');
-        }
-
-        //3) Field limitings
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields)
-        } else {
-            query = query.select('-__v');
-        }
-
-        //4) Pagination
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 20;
-        const skip = (page - 1) * limit;
-
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const numPacks = await Pack.countDocuments();
-            if (skip >= numPacks) throw new Error('This page does not exist');
-        }
-
-        //Execute Query
-        const packs = await query;
-
-        //Send Response
-        res.status(200).json({
-            status: 'success',
-            results: packs.length,
-            data: {
-                packs
-            }
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'Fail',
-            message: err.message
-        });
+    //2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("price");
     }
+
+    //3) Field limitings
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    //4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 20;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numPacks = await Pack.countDocuments();
+      if (skip >= numPacks) throw new Error("This page does not exist");
+    }
+
+    //Execute Query
+    const packs = await query;
+
+    //Send Response
+    res.status(200).json({
+      status: "success",
+      results: packs.length,
+      data: {
+        packs
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "Fail",
+      message: err.message
+    });
+  }
 };
 
 exports.getPack = async (req, res) => {
-    try {
-        const pack = await Pack.findById(req.params.id);
+  try {
+    const pack = await Pack.findById(req.params.id);
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                pack
-            }
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'Fail',
-            message: err
-        });
-    }
+    res.status(200).json({
+      status: "success",
+      data: {
+        pack
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "Fail",
+      message: err
+    });
+  }
 };
 
 exports.createPack = async (req, res) => {
-    try {
-        const newPack = await Pack.create(req.body);
+  try {
+    const newPack = await Pack.create(req.body);
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                pack: newPack
-            }
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'Fail',
-            message: err
-        });
-    }
+    res.status(200).json({
+      status: "success",
+      data: {
+        pack: newPack
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "Fail",
+      message: err
+    });
+  }
 };
 
 exports.updatePack = async (req, res) => {
-    try {
-        const pack = await Pack.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidator: true
-        })
-        res.status(200).json({
-            status: 'success',
-            data: {
-                pack
-            }
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'Fail',
-            message: err.message
-        });
-    }
+  try {
+    const pack = await Pack.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidator: true
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        pack
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "Fail",
+      message: err.message
+    });
+  }
 };
 
 exports.deletePack = async (req, res) => {
-
-    await Pack.findByIdAndDelete(req.params.id);
-    try {
-        res.status(204).json({
-            status: 'success',
-            data: null
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
-    }
+  await Pack.findByIdAndDelete(req.params.id);
+  try {
+    res.status(204).json({
+      status: "success",
+      data: null
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err
+    });
+  }
 };
