4e2f75d6efd656bda2e9502dd0a50051c9061a4d
Michael Frantz <mjfrantz26@gmail.com>
2019-09-18 18:03:23 +0700

    Refactored APIFeatures

--- controllers/packController.js
+++ controllers/packController.js
@@ -1,51 +1,11 @@
 const Pack = require("./../models/packModel");
+const APIFeatures = require("./../utils/apiFeatures");
 
 exports.getAllPacks = async (req, res) => {
   try {
-    //Build Query
-    //1) Filtering
-    const queryObj = {
-      ...req.query
-    };
-    const excludeFields = ["page", "sort", "limit", "fields"];
-    excludeFields.forEach(el => delete queryObj[el]);
-
-    //1b) Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-    let query = Pack.find(JSON.parse(queryStr));
-
-    //2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("price");
-    }
-
-    //3) Field limitings
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
-
-    //4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 20;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numPacks = await Pack.countDocuments();
-      if (skip >= numPacks) throw new Error("This page does not exist");
-    }
-
     //Execute Query
-    const packs = await query;
+    const features = new APIFeatures(Pack.find(), req.query).filter().sort().limitFields().paginate();
+    const packs = await features.query;
 
     //Send Response
     res.status(200).json({
