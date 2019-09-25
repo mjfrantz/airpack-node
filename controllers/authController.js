8a5ed6e71532591c884a3d184dd638875452a58f
Michael Frantz <mjfrantz26@gmail.com>
2019-09-25 14:11:52 +0700

    Log in user with token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
+const appError = require("./../utils/appError");
 
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+}
 
 exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create({
@@ -11,9 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
         passwordConfirm: req.body.passwordConfirm
     })
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    });
+    const token = signToken(newUser._id);
 
     res.status(201).json({
         status: 'success',
@@ -21,5 +25,30 @@ exports.signup = catchAsync(async (req, res, next) => {
         data: {
             user: newUser
         }
+    });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+    //Create two variables of the body object
+    const { email, password } = req.body; 
+
+    //1) Check if email and passwords exists
+    if(!email || !password) {
+      return  next(new appError('Please provide email and password', 400));
+    }
+
+    //2 Check if the user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
+    
+
+    if(!user || !(await user.correctPassword(password, user.password))) {
+        return next(new appError('Incorrect email or password', 401))
+    };
+    
+    //3 If everything is ok, send token to the client
+    const token = signToken(user._id);
+    res.status(200).json({ 
+        status: 'success',
+        token
     });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,8 @@ const authController = require('./../controllers/authController');
 
 
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
 
 router
   .route('/')
