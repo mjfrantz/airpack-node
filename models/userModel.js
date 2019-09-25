dd037756da6d6050cba9afb12474fde16bbc312f
Michael Frantz <mjfrantz26@gmail.com>
2019-09-26 06:48:24 +0700

    Deleting the current user

--- models/userModel.js
+++ models/userModel.js
@@ -42,7 +42,12 @@ const userSchema = new mongoose.Schema ({
     },
     passwordChangedAt: Date,
     passwordResetToken: String,
-    passwordResetExpires: Date
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    }
 });
 
 userSchema.pre('save', async function(next) {
@@ -61,6 +66,12 @@ userSchema.pre('save', function(next){
     if(!this.isModified('password' || this.isNew)) return next();
 
     this.passwordChangedAt = Date.now() - 1000; //ensure the token was created after the pw has been changed!
+    next();
+});
+
+userSchema.pre(/^find/, function(next){
+    //this points to the current query
+    this.find({active: {$ne: false} });
     next();
 });
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,6 +13,8 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
 
 router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
 
 router
   .route('/')
