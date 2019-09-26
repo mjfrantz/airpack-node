d7c50eb429d3201275cb1f813f69e8ad1b908d79
Michael Frantz <mjfrantz26@gmail.com>
2019-09-26 16:39:48 +0700

    Adding Security HTTP headers & Data sanitization

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const cors = require("cors");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -11,14 +14,26 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.json());
-
 //Global Middleware
+
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization again NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
+
+//Set Security HTTP Headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-//Allow 200 request from one IP in one hour 
+//Limit request from same API (Allow 200 request from one IP in one hour) 
 const limiter = rateLimit({
   max: 200,
   windowMs: 60 * 60 * 1000,
@@ -26,6 +41,7 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
