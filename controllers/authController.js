72b5ca2d6efc79a25b552de93990590b2403d67c
Ivan <ivanvlora@gmail.com>
2019-09-27 20:51:06 +0900

    fixed import bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
-const AppError = require("./../utils/AppError");
+const AppError = require("./../utils/appError");
 const sendEmail = require("./../utils/email");
 
 const signToken = id => {
