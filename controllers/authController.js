71dd7009bcf473a217055975c9bcf7fb60288653
Ivan Garcia <ivanvlora@gmail.com>
2019-10-11 17:39:45 +0900

    added prettier config | modified formatting

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,10 @@
-const crypto = require("crypto");
-const { promisify } = require("util");
-const jwt = require("jsonwebtoken");
-const User = require("./../models/userModel");
-const catchAsync = require("./../utils/catchAsync");
-const AppError = require("./../utils/appError");
-const sendEmail = require("./../utils/email");
+const crypto = require('crypto');
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const sendEmail = require('./../utils/email');
 
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -18,17 +18,18 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
+    secure: true,
     httpOnly: true
   };
-  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
-  res.cookie("jwt", token, cookieOptions);
+  res.cookie('jwt', token, cookieOptions);
 
   //Remove password from output
   user.password = undefined;
 
   res.status(statusCode).json({
-    status: "success",
+    status: 'success',
     token,
     data: {
       user
@@ -47,14 +48,14 @@ exports.login = catchAsync(async (req, res, next) => {
 
   //1) Check if email and passwords exists
   if (!email || !password) {
-    return next(new AppError("Please provide email and password", 400));
+    return next(new AppError('Please provide email and password', 400));
   }
 
   //2 Check if the user exists && password is correct
-  const user = await User.findOne({ email }).select("+password");
+  const user = await User.findOne({ email }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError("Incorrect email or password", 401));
+    return next(new AppError('Incorrect email or password', 401));
   }
 
   //3 If everything is ok, send token to the client
@@ -67,14 +68,14 @@ exports.protect = catchAsync(async (req, res, next) => {
   let token;
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer ")
+    req.headers.authorization.startsWith('Bearer ')
   ) {
-    token = req.headers.authorization.split(" ")[1];
+    token = req.headers.authorization.split(' ')[1];
   }
 
   if (!token) {
     return next(
-      new AppError("You are not logged in! Please log in to get access", 401)
+      new AppError('You are not logged in! Please log in to get access', 401)
     );
   }
 
@@ -85,13 +86,13 @@ exports.protect = catchAsync(async (req, res, next) => {
   const currentUser = await User.findById(decoded.id);
   if (!currentUser) {
     return next(
-      new AppError("The user belonging to the token no longer exist!", 401)
+      new AppError('The user belonging to the token no longer exist!', 401)
     );
   }
   //4. Check if user changed password after the token was issued
   if (currentUser.changedPasswordAfter(decoded.iat)) {
     return next(
-      new AppError("User recently changed password! Please log in again.", 401)
+      new AppError('User recently changed password! Please log in again.', 401)
     );
   }
 
@@ -104,7 +105,7 @@ exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return next(
-        new AppError("You do not have permission to perform this action", 403)
+        new AppError('You do not have permission to perform this action', 403)
       );
     }
 
@@ -116,7 +117,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   //1. Get user based on Posted email
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
-    return next(new AppError("There is no user with this email address.", 404));
+    return next(new AppError('There is no user with this email address.', 404));
   }
   //2. Generate the random reset tokens
   const resetToken = user.createPasswordResetToken();
@@ -124,7 +125,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   //3. Send it to user's email
   const resetURL = `${req.protocol}://${req.get(
-    "host"
+    'host'
   )}/api/v1/users/resetPassword/${resetToken}}`;
 
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you did not forget your password, please ignore this email!`;
@@ -132,13 +133,13 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   try {
     await sendEmail({
       email: user.email,
-      subject: "Your password reset token (valid for 10 mins)",
+      subject: 'Your password reset token (valid for 10 mins)',
       message
     });
 
     res.status(200).json({
-      status: "success",
-      message: "Token sent to email!"
+      status: 'success',
+      message: 'Token sent to email!'
     });
   } catch (err) {
     user.passwordResetToken = undefined;
@@ -147,16 +148,16 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 
   return next(
-    new AppError("There was an error sending the email. Try again later!", 500)
+    new AppError('There was an error sending the email. Try again later!', 500)
   );
 });
 
 exports.resetPassword = catchAsync(async (req, res, next) => {
   //1. Get user based on the token provided
   const hashedToken = crypto
-    .createHash("sha256")
+    .createHash('sha256')
     .update(req.params.token)
-    .digest("hex");
+    .digest('hex');
 
   const user = await User.findOne({
     passwordResetToken: hashedToken,
@@ -164,7 +165,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   });
   //2. If token has not expired, and there is a user, set the new password to
   if (!user) {
-    return next(new AppError("Token is invalid or has expired", 400));
+    return next(new AppError('Token is invalid or has expired', 400));
   }
   user.password = req.body.password;
   user.passwordConfirm = req.body.passwordConfirm;
@@ -179,11 +180,11 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
   //1. Get user from collection
-  const user = await User.findById(req.user.id).select("+password");
+  const user = await User.findById(req.user.id).select('+password');
 
   //2. Check if Posted password is correctPassword
   if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
-    return next(new AppError("Your current password is wrong", 401));
+    return next(new AppError('Your current password is wrong', 401));
   }
   //3. If so update the passwords
   user.password = req.body.password;
@@ -196,7 +197,7 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 
 exports.getAuthUser = catchAsync(async (req, res, next) => {
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: {
       user: req.user
     }
--- server.js
+++ server.js
@@ -2,23 +2,26 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({
-    path: './config.env'
+  path: './config.env'
 });
 
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-}).then(() => console.log('DB connection successful'));
-
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
+  console.log(`App running on port ${port}...`);
 });
-
